e0f828ba75d16740ed40913d35100de0d78ea6fe
DevDevi3 <devdavie256@gmail.com>
2026-08-18 22:35:03 +0300

    feat: add a getHumanChoice Function
    this adds a function that allows a user to input his choice for the game

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
  function getComputerChoice()
 {
     //create varable to keep the random number
-    randNum = Math.random();
+    let randNum = Math.random();
 
-    choice = "";
+    let choice = "";
 
     //check  for random number and ssign choice accordng to the random number
     if (randNum <= 0.333)
@@ -24,5 +24,41 @@
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice()
+{
+    // get prayer choice in form of number 
+    let number = parseInt(prompt("Enter Enter Number 1->rock, 2->paper, 3->scissor"));
+    let humanChoice = "";
+    //check if the nmber is from 1-3
+    if (number > 0 && number <= 3)
+    {
+        //assign the number to actual choices
+        if (number === 1)
+        {
+            humanChoice = "Rock";
+        }
+        else if(number === 2)
+        {
+            humanChoice = "Paper";
+        }
+        else
+        {
+            humanChoice = "Scissor";
+        }
+
+        return humanChoice;
+
+    }
+    else
+    {
+        humanChoice = "Invalid input";
+        return humanChoice;
+    }
+    
+    
+}
+
+console.log(getHumanChoice());
 
 
